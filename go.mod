9738df8b5ac8d371e5c5d8d259c5c5cbd62ca142
echometerain <echometer@disroot.org>
2021-11-04 18:29:09 -0400

    initial commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/echometerain/chatapp-amongus
+
+require github.com/bwmarrin/discordgo v0.23.1
+
+go 1.16
---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"github.com/bwmarrin/discordgo"
+)
+
+func main() {
+	discord, err := discordgo.New("Bot " + "authentication token")
+
+}
